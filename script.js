fb0fcd7659be7f5613b4cff7ea3e8b1471dbd55b
Vivek Chauhan <vivekchauhan3597@gmail.com>
2024-12-31 15:06:51 +0530

    uploaded all files

---
+++ script.js
@@ -0,0 +1,114 @@
+const displayOne = document.querySelector('.display-1')
+const displayTwo = document.querySelector('.display-2')
+const allkeys = document.querySelector('.all-btns')
+
+
+let lastKeyPressed = ''
+let operatorKey = ''
+let firstKey = ''
+let secondKey = ''
+
+allkeys.addEventListener('click', (e) => {
+
+    if (!e.target.closest('button')) return
+    const key = e.target
+
+    if (key.dataset.type == 'number') {
+        displayOne.classList.remove('size-small')
+
+        if (lastKeyPressed == 'operator') {
+            displayOne.innerText = ''
+            secondKey = key.innerText
+            displayOne.innerText = secondKey
+            lastKeyPressed = 'number'
+        }
+        else if (lastKeyPressed == 'backspace') {
+            displayOne.innerText += key.innerText
+        }
+        else {
+            displayOne.innerText += key.innerText
+            if (secondKey) {
+                secondKey += key.innerText
+                displayOne.innerText = secondKey
+            }
+        }
+        lastKeyPressed = 'number'
+    }
+
+    if (key.dataset.type == 'operator') {
+        displayTwo.innerText = displayOne.innerText + ' ' + key.innerText
+        operatorKey = key.innerText
+        firstKey = displayOne.innerText
+        lastKeyPressed = 'operator'
+    }
+
+    if (key.dataset.value == 'equal') {
+
+        let firstNumber = Number(firstKey)
+        let secondNumber = Number(displayOne.innerText)
+        let finalResult = calculation(firstNumber, operatorKey, secondNumber)
+
+        if (lastKeyPressed == 'equal' || lastKeyPressed == 'backspace') {            
+            firstNumber = Number(displayOne.innerText)
+            secondNumber = Number(secondKey)
+            finalResult = calculation(firstNumber, operatorKey, secondNumber)
+        }
+        if (typeof finalResult == 'string') {
+            displayOne.classList.add('size-small')
+            displayOne.innerText = finalResult
+        } else {
+            displayOne.innerText = parseFloat(finalResult.toFixed(10))
+        }
+        displayTwo.innerText = `${firstNumber} ${operatorKey} ${secondNumber} =`
+
+        lastKeyPressed = 'equal'
+    }
+
+    if (key.dataset.value == 'clear') {
+        displayOne.innerText = ''
+        displayTwo.innerText = ''
+        firstKey = 0
+        secondKey = 0
+        lastKeyPressed = 'clear'
+    }   
+
+    if (key.dataset.value == 'backspace') {
+
+        if (lastKeyPressed == 'equal') {            
+            displayTwo.innerText = ''
+        } else if (lastKeyPressed == 'backspace' && displayTwo.innerText == '') {
+            console.log('already cleared');
+        }
+        else {
+            const firstNumberInArray = (displayOne.innerText).split('')
+            firstNumberInArray.pop()
+            displayOne.innerText = firstNumberInArray.join('')
+        }
+        lastKeyPressed = 'backspace'
+    }
+
+    if (key.dataset.value == 'negate') {
+        displayOne.innerText = -(Number(displayOne.innerText))
+        lastKeyPressed = 'negate'
+    }
+})
+
+function calculation(firstNumber, operator, secondNumber) {
+    let result = ''
+    if (operator == '+') result = firstNumber + secondNumber
+    if (operator == '-') result = firstNumber - secondNumber
+    if (operator == '×') result = firstNumber * secondNumber
+    if (operator == '%') result = (firstNumber / 100) * secondNumber
+
+    if (operator == '÷') {
+        if (secondNumber === 0) {
+            result = 'Cannot divide by zero'
+        } else {
+            result = firstNumber / secondNumber
+        }
+    }
+    return result
+}
+
+
+
